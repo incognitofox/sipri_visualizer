9763e7ad09abdff0f45b51956df14d1fc59fa25e
William Wang <45670627+incognitofox@users.noreply.github.com>
2021-06-15 14:27:07 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,42 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 14 09:44:13 2021
+from flask import Flask, render_template, request, jsonify
 
-@author: wangwy
-"""
+app = Flask(__name__)
 
-import pandas as pd
-import pygal
-import pycountry_convert as pc
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-def country_to_code(country):
-    try:
-        return pc.country_name_to_country_alpha2(country).lower()
-    except:
-        #print(str(country) + " is not a country.")
-        return
-
-def create_chart(year):
-    df = pd.read_csv("export.csv")
-    df = df.fillna(0)
-    display(df)
-    #df.rename( columns={'Unnamed: 0':'Country'}, inplace=True )
-    #large = max(df[str(year)])
-    #print(large)
-    #df["Total"] = df["Total"].apply(lambda x: 100*x/large)
-    df["COUNTRY NAME"] = df["COUNTRY NAME"].apply(country_to_code)
-    df.dropna()
-    #display(df)
-    df = df[df["YEAR"] == year]
-    #display(df)
-    data = dict(zip(df["COUNTRY NAME"], df["VALUE"]))
-    #print(data)
-    worldmap_chart = pygal.maps.world.World()
-    worldmap_chart.title = 'Agriculture in ' + str(year)
-    worldmap_chart.add("Total", data)
-    #worldmap_chart.render_in_browser()
-    worldmap_chart.render_to_file("static/maps/agriculture/" + str(year)+".svg")
-    return worldmap_chart
- 
-for i in range(2001, 2017):
-    create_chart(i)
+if __name__ == '__main__':
+    app.run()
